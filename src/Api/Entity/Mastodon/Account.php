friendica/friendica

Introduce Mastodon entity factories and API\Entity

src/Api/Entity/Mastodon/Account.php
@@ -1,0 +1,108 @@
+<?php
+
+namespace Friendica\Api\Entity\Mastodon;
+
+use Friendica\Api\Entity;
+use Friendica\App\BaseURL;
+use Friendica\Content\Text\BBCode;
+use Friendica\Database\DBA;
+use Friendica\Model\Contact;
+use Friendica\Util\DateTimeFormat;
+
+/**
+ * Class Account
+ *
+ * @see https://docs.joinmastodon.org/entities/account
+ */
+class Account extends Entity
+{
+	/** @var string */
+	protected $id;
+	/** @var string */
+	protected $username;
+	/** @var string */
+	protected $acct;
+	/** @var string */
+	protected $display_name;
+	/** @var bool */
+	protected $locked;
+	/** @var string (Datetime) */
+	protected $created_at;
+	/** @var int */
+	protected $followers_count;
+	/** @var int */
+	protected $following_count;
+	/** @var int */
+	protected $statuses_count;
+	/** @var string */
+	protected $note;
+	/** @var string (URL)*/
+	protected $url;
+	/** @var string (URL) */
+	protected $avatar;
+	/** @var string (URL) */
+	protected $avatar_static;
+	/** @var string (URL) */
+	protected $header;
+	/** @var string (URL) */
+	protected $header_static;
+	/** @var Emoji[] */
+	protected $emojis;
+	/** @var Account|null */
+	protected $moved = null;
+	/** @var Field[]|null */
+	protected $fields = null;
+	/** @var bool|null */
+	protected $bot = null;
+	/** @var bool */
+	protected $group;
+	/** @var bool */
+	protected $discoverable;
+	/** @var string|null (Datetime) */
+	protected $last_status_at = null;
+
+	/**
+	 * Creates an account record from a public contact record. Expects all contact table fields to be set.
+	 *
+	 * @param BaseURL $baseUrl
+	 * @param array   $publicContact Full contact table record with uid = 0
+	 * @param array   $apcontact     Optional full apcontact table record
+	 * @param array   $userContact   Optional full contact table record with uid != 0
+	 * @throws \Friendica\Network\HTTPException\InternalServerErrorException
+	 */
+	public function __construct(BaseURL $baseUrl, array $publicContact, array $apcontact = [], array $userContact = [])
+	{
+		$this->id              = $publicContact['id'];
+		$this->username        = $publicContact['nick'];
+		$this->acct            =
+			strpos($publicContact['url'], $baseUrl->get() . '/') === 0 ?
+				$publicContact['nick'] :
+				$publicContact['addr'];
+		$this->display_name    = $publicContact['name'];
+		$this->locked          = !empty($apcontact['manually-approve']);
+		$this->created_at      = DateTimeFormat::utc($publicContact['created'], DateTimeFormat::ATOM);
+		$this->followers_count = $apcontact['followers_count'] ?? 0;
+		$this->following_count = $apcontact['following_count'] ?? 0;
+		$this->statuses_count  = $apcontact['statuses_count'] ?? 0;
+		$this->note            = BBCode::convert($publicContact['about'], false);
+		$this->url             = $publicContact['url'];
+		$this->avatar          = $userContact['avatar'] ?? $publicContact['avatar'];
+		$this->avatar_static   = $userContact['avatar'] ?? $publicContact['avatar'];
+		// No header picture in Friendica
+		$this->header          = '';
+		$this->header_static   = '';
+		// No custom emojis per account in Friendica
+		$this->emojis          = [];
+		// No metadata fields in Friendica
+		$this->fields          = [];
+		$this->bot             = ($publicContact['contact-type'] == Contact::TYPE_NEWS);
+		$this->group           = ($publicContact['contact-type'] == Contact::TYPE_COMMUNITY);
+		$this->discoverable    = !$publicContact['unsearchable'];
+
+		$publicContactLastItem = $publicContact['last-item'] ?: DBA::NULL_DATETIME;
+		$userContactLastItem = $userContact['last-item'] ?? DBA::NULL_DATETIME;
+
+		$lastItem = $userContactLastItem > $publicContactLastItem ? $userContactLastItem : $publicContactLastItem;
+		$this->last_status_at  = $lastItem != DBA::NULL_DATETIME ? DateTimeFormat::utc($lastItem, DateTimeFormat::ATOM) : null;
+	}
+}

src/Api/Entity/Mastodon/Field.php
@@ -1,18 +1,20 @@
 <?php
 
-namespace Friendica\Api\Mastodon;
+namespace Friendica\Api\Entity\Mastodon;
+
+use Friendica\Api\Entity;
 
 /**
  * Class Field
  *
  * @see https://docs.joinmastodon.org/api/entities/#field
  */
-class Field
+class Field extends Entity
 {
 	/** @var string */
-	var $name;
+	protected $name;
 	/** @var string (HTML) */
-	var $value;
+	protected $value;
 	/** @var string (Datetime)*/
-	var $verified_at;
+	protected $verified_at;
 }

src/Api/Entity/Mastodon/Instance.php
@@ -1,12 +1,11 @@
 <?php
 
-namespace Friendica\Api\Mastodon;
+namespace Friendica\Api\Entity\Mastodon;
 
-use Friendica\App;
+use Friendica\Api\Entity;
 use Friendica\Core\Config;
 use Friendica\Database\DBA;
 use Friendica\DI;
-use Friendica\Model\APContact;
 use Friendica\Model\User;
 use Friendica\Module\Register;
 
@@ -15,42 +14,41 @@ use Friendica\Module\Register;
  *
  * @see https://docs.joinmastodon.org/api/entities/#instance
  */
-class Instance
+class Instance extends Entity
 {
 	/** @var string (URL) */
-	var $uri;
+	protected $uri;
 	/** @var string */
-	var $title;
+	protected $title;
 	/** @var string */
-	var $description;
+	protected $description;
 	/** @var string */
-	var $email;
+	protected $email;
 	/** @var string */
-	var $version;
+	protected $version;
 	/** @var array */
-	var $urls;
+	protected $urls;
 	/** @var Stats */
-	var $stats;
-	/** @var string */
-	var $thumbnail;
+	protected $stats;
+	/** @var string|null */
+	protected $thumbnail = null;
 	/** @var array */
-	var $languages;
+	protected $languages;
 	/** @var int */
-	var $max_toot_chars;
+	protected $max_toot_chars;
 	/** @var bool */
-	var $registrations;
+	protected $registrations;
 	/** @var bool */
-	var $approval_required;
+	protected $approval_required;
 	/** @var Account|null */
-	var $contact_account;
+	protected $contact_account = null;
 
 	/**
 	 * Creates an instance record
 	 *
-	 * @param App $app
-	 *
 	 * @return Instance
 	 * @throws \Friendica\Network\HTTPException\InternalServerErrorException
+	 * @throws \ImagickException
 	 */
 	public static function get()
 	{
@@ -77,9 +75,8 @@ class Instance
 			$adminList = explode(',', str_replace(' ', '', Config::get('config', 'admin_email')));
 			$administrator = User::getByEmail($adminList[0], ['nickname']);
 			if (!empty($administrator)) {
-				$adminContact = DBA::selectFirst('contact', [], ['nick' => $administrator['nickname'], 'self' => true]);
-				$apcontact = APContact::getByURL($adminContact['url'], false);
-				$instance->contact_account = Account::create($baseUrl, $adminContact, $apcontact);
+				$adminContact = DBA::selectFirst('contact', ['id'], ['nick' => $administrator['nickname'], 'self' => true]);
+				$instance->contact_account = DI::mstdnAccount()->createFromContactId($adminContact['id']);
 			}
 		}
 

src/Api/Entity/Mastodon/Relationship.php
@@ -1,0 +1,60 @@
+<?php
+
+namespace Friendica\Api\Entity\Mastodon;
+
+use Friendica\Api\Entity;
+use Friendica\Model\Contact;
+use Friendica\Util\Network;
+
+/**
+ * Class Relationship
+ *
+ * @see https://docs.joinmastodon.org/api/entities/#relationship
+ */
+class Relationship extends Entity
+{
+	/** @var int */
+	protected $id;
+	/** @var bool */
+	protected $following = false;
+	/** @var bool */
+	protected $followed_by = false;
+	/** @var bool */
+	protected $blocking = false;
+	/** @var bool */
+	protected $muting = false;
+	/** @var bool */
+	protected $muting_notifications = false;
+	/** @var bool */
+	protected $requested = false;
+	/** @var bool */
+	protected $domain_blocking = false;
+	/**
+	 * Unsupported
+	 * @var bool
+	 */
+	protected $showing_reblogs = true;
+	/**
+	 * Unsupported
+	 * @var bool
+	 */
+	protected $endorsed = false;
+
+	/**
+	 * @param int   $userContactId Contact row Id with uid != 0
+	 * @param array $userContact   Full Contact table record with uid != 0
+	 */
+	public function __construct(int $userContactId, array $userContact = [])
+	{
+		$this->id                   = $userContactId;
+		$this->following            = in_array($userContact['rel'] ?? 0, [Contact::SHARING, Contact::FRIEND]);
+		$this->followed_by          = in_array($userContact['rel'] ?? 0, [Contact::FOLLOWER, Contact::FRIEND]);
+		$this->blocking             = (bool)$userContact['blocked'] ?? false;
+		$this->muting               = (bool)$userContact['readonly'] ?? false;
+		$this->muting_notifications = (bool)$userContact['readonly'] ?? false;
+		$this->requested            = (bool)$userContact['pending'] ?? false;
+		$this->domain_blocking      = Network::isUrlBlocked($userContact['url'] ?? '');
+
+		return $this;
+	}
+}

src/Factory/Mastodon/Account.php
@@ -1,0 +1,46 @@
+<?php
+
+namespace Friendica\Factory\Mastodon;
+
+use Friendica\App\BaseURL;
+use Friendica\Model\APContact;
+use Friendica\Model\Contact;
+use Friendica\Network\HTTPException;
+use Friendica\Factory;
+use Psr\Log\LoggerInterface;
+
+class Account extends Factory
+{
+	/** @var BaseURL */
+	protected $baseUrl;
+
+	public function __construct(LoggerInterface $logger, BaseURL $baseURL)
+	{
+		parent::__construct($logger);
+
+		$this->baseUrl = $baseURL;
+	}
+
+	/**
+	 * @param int $contactId
+	 * @param int $uid        User Id
+	 * @return \Friendica\Api\Entity\Mastodon\Account
+	 * @throws HTTPException\InternalServerErrorException
+	 * @throws \ImagickException
+	 */
+	public function createFromContactId(int $contactId, $uid = 0)
+	{
+		$cdata = Contact::getPublicAndUserContacID($contactId, $uid);
+		if (!empty($cdata)) {
+			$publicContact = Contact::getById($cdata['public']);
+			$userContact = Contact::getById($cdata['user']);
+		} else {
+			$publicContact = Contact::getById($contactId);
+			$userContact = [];
+		}
+
+		$apcontact = APContact::getByURL($publicContact['url'], false);
+
+		return new \Friendica\Api\Entity\Mastodon\Account($this->baseUrl, $publicContact, $apcontact, $userContact);
+	}
+}

src/Factory/Mastodon/FollowRequest.php
@@ -1,0 +1,47 @@
+<?php
+
+namespace Friendica\Factory\Mastodon;
+
+use Friendica\App\BaseURL;
+use Friendica\Model\APContact;
+use Friendica\Model\Contact;
+use Friendica\Model\Introduction;
+use Friendica\Network\HTTPException;
+use Friendica\Factory;
+use Psr\Log\LoggerInterface;
+
+class FollowRequest extends Factory
+{
+	/** @var BaseURL */
+	protected $baseUrl;
+
+	public function __construct(LoggerInterface $logger, BaseURL $baseURL)
+	{
+		parent::__construct($logger);
+
+		$this->baseUrl = $baseURL;
+	}
+
+	/**
+	 * @param Introduction $Introduction
+	 * @return \Friendica\Api\Entity\Mastodon\FollowRequest
+	 * @throws HTTPException\InternalServerErrorException
+	 * @throws \ImagickException
+	 */
+	public function createFromIntroduction(Introduction $Introduction)
+	{
+		$cdata = Contact::getPublicAndUserContacID($Introduction->{'contact-id'}, $Introduction->uid);
+
+		if (empty($cdata)) {
+			$this->logger->warning('Wrong introduction data', ['Introduction' => $Introduction]);
+			throw new HTTPException\InternalServerErrorException('Wrong introduction data');
+		}
+
+		$publicContact = Contact::getById($cdata['public']);
+		$userContact = Contact::getById($cdata['user']);
+
+		$apcontact = APContact::getByURL($publicContact['url'], false);
+
+		return new \Friendica\Api\Entity\Mastodon\FollowRequest($this->baseUrl, $Introduction->id, $publicContact, $apcontact, $userContact);
+	}
+}

src/Factory/Mastodon/Relationship.php
@@ -1,0 +1,38 @@
+<?php
+
+namespace Friendica\Factory\Mastodon;
+
+use Friendica\Api\Entity\Mastodon\Relationship as RelationshipEntity;
+use Friendica\Factory;
+use Friendica\Model\Contact;
+
+class Relationship extends Factory
+{
+	/**
+	 * @param int $userContactId Contact row id with uid != 0
+	 * @return RelationshipEntity
+	 * @throws \Exception
+	 */
+	public function createFromContactId(int $userContactId)
+	{
+		return $this->createFromContact(Contact::getById($userContactId));
+	}
+
+	/**
+	 * @param array $userContact Full contact row record with uid != 0
+	 * @return RelationshipEntity
+	 */
+	public function createFromContact(array $userContact)
+	{
+		return new RelationshipEntity($userContact['id'], $userContact);
+	}
+
+	/**
+	 * @param int $userContactId Contact row id with uid != 0
+	 * @return RelationshipEntity
+	 */
+	public function createDefaultFromContactId(int $userContactId)
+	{
+		return new RelationshipEntity($userContactId);
+	}
+}

eblessings/doctors-eblessings

src/Api/Entity/Mastodon/Account.php
@@ -1,0 +1,108 @@
+<?php
+
+namespace Friendica\Api\Entity\Mastodon;
+
+use Friendica\Api\BaseEntity;
+use Friendica\App\BaseURL;
+use Friendica\Content\Text\BBCode;
+use Friendica\Database\DBA;
+use Friendica\Model\Contact;
+use Friendica\Util\DateTimeFormat;
+
+/**
+ * Class Account
+ *
+ * @see https://docs.joinmastodon.org/entities/account
+ */
+class Account extends BaseEntity
+{
+	/** @var string */
+	protected $id;
+	/** @var string */
+	protected $username;
+	/** @var string */
+	protected $acct;
+	/** @var string */
+	protected $display_name;
+	/** @var bool */
+	protected $locked;
+	/** @var string (Datetime) */
+	protected $created_at;
+	/** @var int */
+	protected $followers_count;
+	/** @var int */
+	protected $following_count;
+	/** @var int */
+	protected $statuses_count;
+	/** @var string */
+	protected $note;
+	/** @var string (URL)*/
+	protected $url;
+	/** @var string (URL) */
+	protected $avatar;
+	/** @var string (URL) */
+	protected $avatar_static;
+	/** @var string (URL) */
+	protected $header;
+	/** @var string (URL) */
+	protected $header_static;
+	/** @var Emoji[] */
+	protected $emojis;
+	/** @var Account|null */
+	protected $moved = null;
+	/** @var Field[]|null */
+	protected $fields = null;
+	/** @var bool|null */
+	protected $bot = null;
+	/** @var bool */
+	protected $group;
+	/** @var bool */
+	protected $discoverable;
+	/** @var string|null (Datetime) */
+	protected $last_status_at = null;
+
+	/**
+	 * Creates an account record from a public contact record. Expects all contact table fields to be set.
+	 *
+	 * @param BaseURL $baseUrl
+	 * @param array   $publicContact Full contact table record with uid = 0
+	 * @param array   $apcontact     Optional full apcontact table record
+	 * @param array   $userContact   Optional full contact table record with uid != 0
+	 * @throws \Friendica\Network\HTTPException\InternalServerErrorException
+	 */
+	public function __construct(BaseURL $baseUrl, array $publicContact, array $apcontact = [], array $userContact = [])
+	{
+		$this->id              = $publicContact['id'];
+		$this->username        = $publicContact['nick'];
+		$this->acct            =
+			strpos($publicContact['url'], $baseUrl->get() . '/') === 0 ?
+				$publicContact['nick'] :
+				$publicContact['addr'];
+		$this->display_name    = $publicContact['name'];
+		$this->locked          = !empty($apcontact['manually-approve']);
+		$this->created_at      = DateTimeFormat::utc($publicContact['created'], DateTimeFormat::ATOM);
+		$this->followers_count = $apcontact['followers_count'] ?? 0;
+		$this->following_count = $apcontact['following_count'] ?? 0;
+		$this->statuses_count  = $apcontact['statuses_count'] ?? 0;
+		$this->note            = BBCode::convert($publicContact['about'], false);
+		$this->url             = $publicContact['url'];
+		$this->avatar          = $userContact['avatar'] ?? $publicContact['avatar'];
+		$this->avatar_static   = $userContact['avatar'] ?? $publicContact['avatar'];
+		// No header picture in Friendica
+		$this->header          = '';
+		$this->header_static   = '';
+		// No custom emojis per account in Friendica
+		$this->emojis          = [];
+		// No metadata fields in Friendica
+		$this->fields          = [];
+		$this->bot             = ($publicContact['contact-type'] == Contact::TYPE_NEWS);
+		$this->group           = ($publicContact['contact-type'] == Contact::TYPE_COMMUNITY);
+		$this->discoverable    = !$publicContact['unsearchable'];
+
+		$publicContactLastItem = $publicContact['last-item'] ?: DBA::NULL_DATETIME;
+		$userContactLastItem = $userContact['last-item'] ?? DBA::NULL_DATETIME;
+
+		$lastItem = $userContactLastItem > $publicContactLastItem ? $userContactLastItem : $publicContactLastItem;
+		$this->last_status_at  = $lastItem != DBA::NULL_DATETIME ? DateTimeFormat::utc($lastItem, DateTimeFormat::ATOM) : null;
+	}
+}

src/Api/Entity/Mastodon/Field.php
@@ -1,18 +1,20 @@
 <?php
 
-namespace Friendica\Api\Mastodon;
+namespace Friendica\Api\Entity\Mastodon;
+
+use Friendica\Api\BaseEntity;
 
 /**
  * Class Field
  *
  * @see https://docs.joinmastodon.org/api/entities/#field
  */
-class Field
+class Field extends BaseEntity
 {
 	/** @var string */
-	var $name;
+	protected $name;
 	/** @var string (HTML) */
-	var $value;
+	protected $value;
 	/** @var string (Datetime)*/
-	var $verified_at;
+	protected $verified_at;
 }

src/Api/Entity/Mastodon/Instance.php
@@ -1,12 +1,11 @@
 <?php
 
-namespace Friendica\Api\Mastodon;
+namespace Friendica\Api\Entity\Mastodon;
 
-use Friendica\App;
+use Friendica\Api\BaseEntity;
 use Friendica\Core\Config;
 use Friendica\Database\DBA;
 use Friendica\DI;
-use Friendica\Model\APContact;
 use Friendica\Model\User;
 use Friendica\Module\Register;
 
@@ -15,42 +14,41 @@ use Friendica\Module\Register;
  *
  * @see https://docs.joinmastodon.org/api/entities/#instance
  */
-class Instance
+class Instance extends BaseEntity
 {
 	/** @var string (URL) */
-	var $uri;
+	protected $uri;
 	/** @var string */
-	var $title;
+	protected $title;
 	/** @var string */
-	var $description;
+	protected $description;
 	/** @var string */
-	var $email;
+	protected $email;
 	/** @var string */
-	var $version;
+	protected $version;
 	/** @var array */
-	var $urls;
+	protected $urls;
 	/** @var Stats */
-	var $stats;
-	/** @var string */
-	var $thumbnail;
+	protected $stats;
+	/** @var string|null */
+	protected $thumbnail = null;
 	/** @var array */
-	var $languages;
+	protected $languages;
 	/** @var int */
-	var $max_toot_chars;
+	protected $max_toot_chars;
 	/** @var bool */
-	var $registrations;
+	protected $registrations;
 	/** @var bool */
-	var $approval_required;
+	protected $approval_required;
 	/** @var Account|null */
-	var $contact_account;
+	protected $contact_account = null;
 
 	/**
 	 * Creates an instance record
 	 *
-	 * @param App $app
-	 *
 	 * @return Instance
 	 * @throws \Friendica\Network\HTTPException\InternalServerErrorException
+	 * @throws \ImagickException
 	 */
 	public static function get()
 	{
@@ -77,9 +75,8 @@ class Instance
 			$adminList = explode(',', str_replace(' ', '', Config::get('config', 'admin_email')));
 			$administrator = User::getByEmail($adminList[0], ['nickname']);
 			if (!empty($administrator)) {
-				$adminContact = DBA::selectFirst('contact', [], ['nick' => $administrator['nickname'], 'self' => true]);
-				$apcontact = APContact::getByURL($adminContact['url'], false);
-				$instance->contact_account = Account::create($baseUrl, $adminContact, $apcontact);
+				$adminContact = DBA::selectFirst('contact', ['id'], ['nick' => $administrator['nickname'], 'self' => true]);
+				$instance->contact_account = DI::mstdnAccount()->createFromContactId($adminContact['id']);
 			}
 		}
 

src/Api/Entity/Mastodon/Relationship.php
@@ -1,0 +1,60 @@
+<?php
+
+namespace Friendica\Api\Entity\Mastodon;
+
+use Friendica\Api\BaseEntity;
+use Friendica\Model\Contact;
+use Friendica\Util\Network;
+
+/**
+ * Class Relationship
+ *
+ * @see https://docs.joinmastodon.org/api/entities/#relationship
+ */
+class Relationship extends BaseEntity
+{
+	/** @var int */
+	protected $id;
+	/** @var bool */
+	protected $following = false;
+	/** @var bool */
+	protected $followed_by = false;
+	/** @var bool */
+	protected $blocking = false;
+	/** @var bool */
+	protected $muting = false;
+	/** @var bool */
+	protected $muting_notifications = false;
+	/** @var bool */
+	protected $requested = false;
+	/** @var bool */
+	protected $domain_blocking = false;
+	/**
+	 * Unsupported
+	 * @var bool
+	 */
+	protected $showing_reblogs = true;
+	/**
+	 * Unsupported
+	 * @var bool
+	 */
+	protected $endorsed = false;
+
+	/**
+	 * @param int   $userContactId Contact row Id with uid != 0
+	 * @param array $userContact   Full Contact table record with uid != 0
+	 */
+	public function __construct(int $userContactId, array $userContact = [])
+	{
+		$this->id                   = $userContactId;
+		$this->following            = in_array($userContact['rel'] ?? 0, [Contact::SHARING, Contact::FRIEND]);
+		$this->followed_by          = in_array($userContact['rel'] ?? 0, [Contact::FOLLOWER, Contact::FRIEND]);
+		$this->blocking             = (bool)$userContact['blocked'] ?? false;
+		$this->muting               = (bool)$userContact['readonly'] ?? false;
+		$this->muting_notifications = (bool)$userContact['readonly'] ?? false;
+		$this->requested            = (bool)$userContact['pending'] ?? false;
+		$this->domain_blocking      = Network::isUrlBlocked($userContact['url'] ?? '');
+
+		return $this;
+	}
+}

src/Factory/Mastodon/Account.php
@@ -1,0 +1,46 @@
+<?php
+
+namespace Friendica\Factory\Mastodon;
+
+use Friendica\App\BaseURL;
+use Friendica\Model\APContact;
+use Friendica\Model\Contact;
+use Friendica\Network\HTTPException;
+use Friendica\BaseFactory;
+use Psr\Log\LoggerInterface;
+
+class Account extends BaseFactory
+{
+	/** @var BaseURL */
+	protected $baseUrl;
+
+	public function __construct(LoggerInterface $logger, BaseURL $baseURL)
+	{
+		parent::__construct($logger);
+
+		$this->baseUrl = $baseURL;
+	}
+
+	/**
+	 * @param int $contactId
+	 * @param int $uid        User Id
+	 * @return \Friendica\Api\Entity\Mastodon\Account
+	 * @throws HTTPException\InternalServerErrorException
+	 * @throws \ImagickException
+	 */
+	public function createFromContactId(int $contactId, $uid = 0)
+	{
+		$cdata = Contact::getPublicAndUserContacID($contactId, $uid);
+		if (!empty($cdata)) {
+			$publicContact = Contact::getById($cdata['public']);
+			$userContact = Contact::getById($cdata['user']);
+		} else {
+			$publicContact = Contact::getById($contactId);
+			$userContact = [];
+		}
+
+		$apcontact = APContact::getByURL($publicContact['url'], false);
+
+		return new \Friendica\Api\Entity\Mastodon\Account($this->baseUrl, $publicContact, $apcontact, $userContact);
+	}
+}

src/Factory/Mastodon/FollowRequest.php
@@ -1,0 +1,47 @@
+<?php
+
+namespace Friendica\Factory\Mastodon;
+
+use Friendica\App\BaseURL;
+use Friendica\Model\APContact;
+use Friendica\Model\Contact;
+use Friendica\Model\Introduction;
+use Friendica\Network\HTTPException;
+use Friendica\BaseFactory;
+use Psr\Log\LoggerInterface;
+
+class FollowRequest extends BaseFactory
+{
+	/** @var BaseURL */
+	protected $baseUrl;
+
+	public function __construct(LoggerInterface $logger, BaseURL $baseURL)
+	{
+		parent::__construct($logger);
+
+		$this->baseUrl = $baseURL;
+	}
+
+	/**
+	 * @param Introduction $Introduction
+	 * @return \Friendica\Api\Entity\Mastodon\FollowRequest
+	 * @throws HTTPException\InternalServerErrorException
+	 * @throws \ImagickException
+	 */
+	public function createFromIntroduction(Introduction $Introduction)
+	{
+		$cdata = Contact::getPublicAndUserContacID($Introduction->{'contact-id'}, $Introduction->uid);
+
+		if (empty($cdata)) {
+			$this->logger->warning('Wrong introduction data', ['Introduction' => $Introduction]);
+			throw new HTTPException\InternalServerErrorException('Wrong introduction data');
+		}
+
+		$publicContact = Contact::getById($cdata['public']);
+		$userContact = Contact::getById($cdata['user']);
+
+		$apcontact = APContact::getByURL($publicContact['url'], false);
+
+		return new \Friendica\Api\Entity\Mastodon\FollowRequest($this->baseUrl, $Introduction->id, $publicContact, $apcontact, $userContact);
+	}
+}

src/Factory/Mastodon/Relationship.php
@@ -1,0 +1,38 @@
+<?php
+
+namespace Friendica\Factory\Mastodon;
+
+use Friendica\Api\Entity\Mastodon\Relationship as RelationshipEntity;
+use Friendica\BaseFactory;
+use Friendica\Model\Contact;
+
+class Relationship extends BaseFactory
+{
+	/**
+	 * @param int $userContactId Contact row id with uid != 0
+	 * @return RelationshipEntity
+	 * @throws \Exception
+	 */
+	public function createFromContactId(int $userContactId)
+	{
+		return $this->createFromContact(Contact::getById($userContactId));
+	}
+
+	/**
+	 * @param array $userContact Full contact row record with uid != 0
+	 * @return RelationshipEntity
+	 */
+	public function createFromContact(array $userContact)
+	{
+		return new RelationshipEntity($userContact['id'], $userContact);
+	}
+
+	/**
+	 * @param int $userContactId Contact row id with uid != 0
+	 * @return RelationshipEntity
+	 */
+	public function createDefaultFromContactId(int $userContactId)
+	{
+		return new RelationshipEntity($userContactId);
+	}
+}